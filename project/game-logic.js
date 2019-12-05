fb13d32a687ccab37a6ea49a57e2326d784db897
hno1993 <44380685+hno1993@users.noreply.github.com>
2019-12-05 09:32:48 +0300

    update src

--- project/game-logic.js
+++ project/game-logic.js
@@ -43,7 +43,7 @@ bytutorialHTML5Game = {
 		
 		//load the images and image cover
 		for(var i = 1; i <= noOfBoxGame; i++){
-			boxes += "<div id='box-" + i + "' class='box-picture'><img src='img/" + (parseInt(boxIndexes[i-1]) + 1) + ".jpg'/></div>";
+			boxes += "<div id='box-" + i + "' class='box-picture'><img src='img/numbers/" + (parseInt(boxIndexes[i-1]) + 1) + ".png'/></div>";
 			boxCover += "<div id='box-cover-" + i + "' class='box-cover' data-id='" + (parseInt(boxIndexes[i-1]) + 1) + "'></div>";
 		}
 		boxCover = "<div class='box-cover-wrapper'>" + boxCover + "</div>";
